66309c863f2a8669ee62a910ab839e1e4874668b
monicapr1 <monicapr858@gmail.com>
2023-04-19 22:29:51 -0600

    buscadorS

--- practicaSPO/js/index.js
+++ practicaSPO/js/index.js
@@ -1,7 +1,10 @@
+function searchAlbum() {
 let nombreArtista = document.getElementById('nombreArtista');
 let nombreAlbum = document.getElementById('nombreAlbum');
 let fotoArtista = document.getElementById('fotoArtista');
 let listaCanciones = document.getElementById('listaCanciones');
+const artistId = document.getElementById('artist-id').value;
+const url = `https://spotify23.p.rapidapi.com/albums/?ids=${artistId}`;
 
 const options = {
     method: 'GET',
@@ -12,30 +15,32 @@ const options = {
 };
 
 
-fetch('https://spotify23.p.rapidapi.com/albums/?ids=3Mm1P5CEEAiuJqBclr2EyU', options)
-    .then(response => response.json())
-    .then(response => {
-        nombreArtista.innerHTML = response.albums[0].artists[0].name;
-        nombreAlbum.innerHTML = response.albums[0].name;
-        fotoArtista.src = response.albums[0].images[0].url;
+fetch(url, options)
+.then(response => response.json())
+.then(response => {
+    nombreArtista.innerHTML = response.albums[0].artists[0].name;
+    nombreAlbum.innerHTML = response.albums[0].name;
+    fotoArtista.src = response.albums[0].images[0].url;
 
-        let canciones = response.albums[0].tracks.items;
-        
-        let todasLasCanciones = '';
-        canciones.forEach((cancion,index)=>{
-            let nombreCancion = cancion.name;
-            let cancionUrl = cancion.preview_url;
-            todasLasCanciones += `
-            <div class="col-12 col-md-4 text-center">
-                <p>${nombreCancion}</p>
-                <audio controls>
-                    <source src="${cancionUrl}" type="audio/mpeg">
-                    Your browser does not support the audio element.
-                </audio>
-            </div>`;
-            console.log(nombreCancion);
-            console.log(cancion.preview_url);
-        });
-        listaCanciones.innerHTML = todasLasCanciones;
-    })
-    .catch(err => console.error(err));
+    let canciones = response.albums[0].tracks.items;
+    
+    let todasLasCanciones = '';
+    canciones.forEach((cancion,index)=>{
+        let nombreCancion = cancion.name;
+        let cancionUrl = cancion.preview_url;
+        todasLasCanciones += `
+        <div class="col-12 col-md-4 text-center">
+            <p>${nombreCancion}</p>
+            <audio controls>
+                <source src="${cancionUrl}" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        </div>`;
+        console.log(nombreCancion);
+        console.log(cancion.preview_url);
+    });
+    listaCanciones.innerHTML = todasLasCanciones;
+})
+.catch(err => console.error(err));
+
+}
